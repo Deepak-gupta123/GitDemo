14429ea948e6b971273466ba3335426ccd9f1481
Deepak <dkumar392252@gmail.com>
2021-07-30 15:36:14 -0400

    This is QA Engineer Commit

--- GitDemo/src/loginFunctionality/Test2.java
+++ GitDemo/src/loginFunctionality/Test2.java
@@ -1,10 +1,11 @@
 package loginFunctionality;
 
-public class Test1 {
+public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("This is Test Lead Test");
 		
+		
+		System.out.println("This is QA Engineer Test");
 
 	}
 
